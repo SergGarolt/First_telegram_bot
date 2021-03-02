8a337d51bee09c99b6ee8451629eb098bd5e57a7
SergGarolt <76477878+SergGarolt@users.noreply.github.com>
2021-03-02 14:15:04 +0300

    Add files via upload

--- bot-main.py
+++ bot-main.py
@@ -11,12 +11,16 @@ def main():
 
     handler = MessageHandler(Filters.all, do_echo)
     start_handler = CommandHandler('start', do_start)
+    help_handler = CommandHandler('help', do_help) # Это второй шаг создания команды
 
     dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler) # Это третий, самый важный этап создания команды - регистрация.
+    # Важно регистрировать команды в правильном порядке - если поставить эту регистрацию после той, что реагирует на все незарегестрированные команды - эта команда обработается как незарегестрированная
+    # Диспетчер бота прогоняет команду по функциям и "скармливает" ее первой подошедшей для команды
     dispatcher.add_handler(handler)
 
     updater.start_polling()
-    updater.idle()
+    updater.idle()  # Эти две строки запускают бота
 
 def do_echo(update, context):
 
@@ -29,6 +33,14 @@ def do_start(update, context):
 
 
     update.message.reply_text(text='Приветствую! Как насчет похода на Иерусалим?')
+    # Сама суть функции - принять два параметра и среагировать на что-то, дальше по коду.
+    # Создание функции - первый шаг к созданию обрабатываемой команды
+
+
+def do_help(update, context):
+
+
+    update.message.reply_text(text='Я пока не умею тебе отвечать нормально, только шутки про крестовые походы :)')
 
 
 main()
