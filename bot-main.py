c8cbb195bc29c4f9cf036358f8fe96f6bf7f4b95
SergGarolt <76477878+SergGarolt@users.noreply.github.com>
2021-03-03 14:11:56 +0300

    Add files via upload

--- bot-main.py
+++ bot-main.py
@@ -1,3 +1,4 @@
+from telegram import Update
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
 
@@ -10,13 +11,15 @@ def main():
     dispatcher = updater.dispatcher
 
     handler = MessageHandler(Filters.all, do_echo)
+    cod_info_handler = CommandHandler('cod_info', do_cod_info)
     start_handler = CommandHandler('start', do_start)
-    help_handler = CommandHandler('help', do_help) # Это второй шаг создания команды
+    help_handler = CommandHandler('help', do_help)  # Это второй шаг создания команды
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler) # Это третий, самый важный этап создания команды - регистрация.
     # Важно регистрировать команды в правильном порядке - если поставить эту регистрацию после той, что реагирует на все незарегестрированные команды - эта команда обработается как незарегестрированная
     # Диспетчер бота прогоняет команду по функциям и "скармливает" ее первой подошедшей для команды
+    dispatcher.add_handler(cod_info_handler)
     dispatcher.add_handler(handler)
 
     updater.start_polling()
@@ -37,10 +40,18 @@ def do_start(update, context):
     # Создание функции - первый шаг к созданию обрабатываемой команды
 
 
-def do_help(update, context):
+def do_help(update: Update, context):
+
+    user_id = update.message.from_user.id
+    name = update.message.from_user.first_name
+    update.message.reply_text(text=f'Привет, {name}! Я пока не умею тебе отвечать нормально, только шутки про крестовые походы :)')
 
 
-    update.message.reply_text(text='Я пока не умею тебе отвечать нормально, только шутки про крестовые походы :)')
+def do_cod_info(update: Update, context):
+
+    name = update.message.from_user.first_name
+    username = update.message.from_user.username
+    update.message.reply_text(text=f'Кодовое имя: {username}.\nПозывной: {name}. \nКомандные совместные боевые операции.')
 
 
 main()
