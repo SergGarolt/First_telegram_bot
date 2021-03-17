a43f87e13e9e0e4f415efd1e960b4b0cc1699ed8
SergGarolt <76477878+SergGarolt@users.noreply.github.com>
2021-03-17 14:13:41 +0300

    Add files via upload

--- Exsel.py
+++ Exsel.py
@@ -2,8 +2,11 @@ from openpyxl import load_workbook
 
 
 book = load_workbook('Test.xlsx')
-sheet = book['Стикеры']
- # sheet['A1'].value = 1
+sheet_1 = book['Стикеры']
+sticker_page = book['Стикеры']
 
 print(book.worksheets)
-print(sheet['A1'].value)
+print(sticker_page['A1'].value)
+for i in range(1, 4):
+    print(sticker_page.cell(row=1, column=i).value)
+
