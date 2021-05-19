b72521aa353d8fad9cab79487cedb806da94e5b7
SergGarolt <76477878+SergGarolt@users.noreply.github.com>
2021-05-19 14:22:38 +0300

    Add files via upload

--- gamelist-bot.py
+++ gamelist-bot.py
@@ -18,9 +18,11 @@ def main():
     start_handler = CommandHandler('start', do_start)
     keybord2_handler = MessageHandler(Filters.text('Вернуться к списку игр'), do_start)
     keybord_handler = MessageHandler(Filters.text, do_something)
+    sis_handler = MessageHandler(Filters.text('Минимальные системные требования'), do_write_sis)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(keybord2_handler)
+    dispatcher.add_handler(sis_handler)
     dispatcher.add_handler(keybord_handler)
     dispatcher.add_handler(handler)
 
@@ -52,6 +54,7 @@ def do_something(update: Update, context):
         print(f'{catch_phrase=}')
         print(f'{text=}')
         if catch_phrase in text:
+            context.user_data['Game'] = text
             price = gameinfo_page.cell(row=row, column=5).value
             store = gameinfo_page.cell(row=row, column=4).value
             genre = gameinfo_page.cell(row=row, column=2).value
@@ -70,5 +73,31 @@ def do_something(update: Update, context):
             break
 
     #доделать вторую клавиатуру
+def do_write_sis (update: Update, context):
+    text = context.user_data['Game']
+
+    for row in range(1, gameinfo_page.max_row + 1):
+        catch_phrase = gameinfo_page.cell(row=row, column=1).value
+        print(f'{row=}')
+        print(f'{catch_phrase=}')
+        print(f'{text=}')
+        if catch_phrase in text:
+            CPU = gameinfo_page.cell(row=row, column=7).value
+            RAM = gameinfo_page.cell(row=row, column=8).value
+            graphics_card = gameinfo_page.cell(row=row, column=9).value
+            operating_system = gameinfo_page.cell(row=row, column=6).value
+            video_card_memory = gameinfo_page.cell(row=row, column=11).value
+            DirectX = gameinfo_page.cell(row=row, column=12).value
+            HDD = gameinfo_page.cell(row=row, column=13).value
+
+            update.message.reply_text(f'Оперативная система: {operating_system}')
+            update.message.reply_text(f'Процессор: {CPU}')
+            update.message.reply_text(f'ОЗУ: {RAM}')
+            update.message.reply_text(f'Видеокарта: {graphics_card}')
+            update.message.reply_text(f'Память видеокарты: {video_card_memory}')
+            update.message.reply_text(f'DirectX: {DirectX}')
+            update.message.reply_text(f'Память на диске: {HDD}')
+
+    return do_start
 
 main()
